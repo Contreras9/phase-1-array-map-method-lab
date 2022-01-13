aa6f24794bbecafe05050c1ed82b0829018bc5a7
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-12 19:00:05 -0600

    commit tree a49c50cd733269fc591a85c90152385d488af46c

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
 
-function titleCased() {
-  return tutorials.map(makeCapital);
-}
+// function titleCased() {
+//   return tutorials.map(makeCapital);
+// }
 
 
-const makeCapital = (title) => {
-  let words = title.split(" ");
-  let capitalWords = words.map(function(element) {return element[0].toUpperCase() + element.slice(1)})
-  return capitalWords.join(" ");
-}
+// const makeCapital = (title) => {
+//   let words = title.split(" ");
+//   let capitalWords = words.map(function(element) {return element[0].toUpperCase() + element.slice(1)})
+//   return capitalWords.join(" ");
+// }
